5fa41bf18926208ab2096f14356a52e6115d5f05
mike <mikethecodegeek@gmail.com>
2021-02-25 13:22:02 -0800

    portfolio updated

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // $('.nav-home').addClass('active');
-   $('.hamburger').on('click',function() {
-       $('#mobile-menu').slideToggle(100, 'linear').addClass('only-mobile');
-   })
-
-});
-
